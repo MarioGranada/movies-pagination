dcfcc29e54df68d7db27478915f4a331ea8e2237
MarioGranada <mario.hernandez@yolo.com>
2025-10-27 13:07:01 +0200

    minor updates

--- src/shared/context/MovieContext/MovieContextWrapper.tsx
+++ src/shared/context/MovieContext/MovieContextWrapper.tsx
@@ -1,8 +1,6 @@
-// import { useEffect, useState, type FC } from "react";
 import { useEffect, type FC } from "react";
 
 import customFetch from "../../helpers/customFetch";
-// import MovieContext, { initContextValue } from ".";
 import { useDispatch } from "react-redux";
 import { addConfig, addGenres } from "../../../store/configSlice";
 
@@ -11,49 +9,6 @@ type Props = {
 };
 
 const MovieContextWrapper: FC<Props> = ({ children }) => {
-  // const [moviesConfig, setMoviesConfig] = useState(initContextValue);
-  // const url = "https://api.themoviedb.org/3/configuration";
-
-  // useEffect(() => {
-  //   const abortController = new AbortController();
-  //   const options = {
-  //     signal: abortController.signal,
-  //   };
-
-  //   const fetchConfig = async () => {
-  //     const response = await customFetch(url, undefined, options);
-  //     setMoviesConfig((prevState) => ({
-  //       ...prevState,
-  //       config: { ...response },
-  //     }));
-  //   };
-
-  //   const fetchGenres = async () => {
-  //     const genresUrl = "https://api.themoviedb.org/3/genre/movie/list";
-
-  //     const response = await customFetch(genresUrl, undefined, options);
-  //     const { genres } = response;
-  //     const genresMap: GenreMap = {};
-
-  //     genres.forEach((genre: Genre) => {
-  //       genresMap[genre.id] = genre.name;
-  //     });
-
-  //     setMoviesConfig((prevState) => ({
-  //       ...prevState,
-  //       genres: genresMap,
-  //     }));
-  //   };
-
-  //   fetchConfig();
-  //   fetchGenres();
-
-  //   return () => {
-  //     abortController.abort();
-  //   };
-  // }, []);
-
-  // const [moviesConfig, setMoviesConfig] = useState(initContextValue);
   const url = "https://api.themoviedb.org/3/configuration";
 
   const dispatch = useDispatch();
@@ -66,10 +21,6 @@ const MovieContextWrapper: FC<Props> = ({ children }) => {
 
     const fetchConfig = async () => {
       const response = await customFetch(url, undefined, options);
-      // setMoviesConfig((prevState) => ({
-      //   ...prevState,
-      //   config: { ...response },
-      // }));
 
       dispatch(addConfig({ ...response }));
     };
@@ -85,11 +36,6 @@ const MovieContextWrapper: FC<Props> = ({ children }) => {
         genresMap[genre.id] = genre.name;
       });
 
-      // setMoviesConfig((prevState) => ({
-      //   ...prevState,
-      //   genres: genresMap,
-      // }));
-
       dispatch(addGenres({ genres: genresMap }));
     };
 
@@ -102,7 +48,6 @@ const MovieContextWrapper: FC<Props> = ({ children }) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  // return <MovieContext value={moviesConfig}>{children}</MovieContext>;
   return <> {children}</>;
 };
 
